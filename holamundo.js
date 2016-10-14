84b754caa9dea7fb047de008b12097e9e0851a21
Miguel Gastalver <mgastalver@gmail.com>
2016-10-14 23:00:01 +0200

    holamundo

--- holamundo.js
+++ holamundo.js
@@ -1,3 +1,11 @@
 /**
  * Created by Miguel on 14/10/16.
  */
+
+var five = require('johnny-five');
+var board = new five.Board();
+
+board.on("ready", function(){   // Equivalente a void loop() en C
+    var led = new five.Led(13);
+    led.blink(100);
+})
